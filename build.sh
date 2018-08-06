18bec3440f28b09fa6a63142ba04b9958bc53114
Gerry Weißbach <gweissbach@inetsoftware.de>
2018-08-06 15:57:18 +0200

    Next version.

--- build.sh
+++ build.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-
-brew update && brew bundle
-
-if [ -d "~/projects" ]; then
-    rm -rf "~/projects"
-fi
-
-mkdir "~/projects"
-cd "~/projects"
-
-git clone --depth 1 "https://github.com/tesseract-ocr/tesseract/"
+echo "####### Clone"
+git clone "https://github.com/tesseract-ocr/tesseract/"
 cd tesseract/java
 
 wget "http://search.maven.org/remotecontent?filepath=org/piccolo2d/piccolo2d-core/3.0/piccolo2d-core-3.0.jar"
 wget "http://search.maven.org/remotecontent?filepath=org/piccolo2d/piccolo2d-extras/3.0/piccolo2d-extras-3.0.jar"
-git clone --depth 1 "https://github.com/tesseract-ocr/langdata"
-
+git clone "https://github.com/tesseract-ocr/langdata"
 
 cd -
 cd "tesseract"
 
+echo "####### Configure"
 ./autogen.sh
 ./configure CPPFLAGS=-I/usr/local/opt/icu4c/include LDFLAGS=-L/usr/local/opt/icu4c/lib
 
+echo "####### Make"
 make -j
 make install
 update_dyld_shared_cache
 
+echo "####### Make Training"
 make training
 
 cd -
@@ -37,11 +30,14 @@ SCROLLVIEW_PATH=$(pwd) make ScrollView.jar
 
 cd -
 
+echo "####### Text2Image"
 text2image --list_available_fonts --fonts_dir=/Library/Fonts
 
-git clone --depth 1 "https://github.com/tesseract-ocr/tessdata_best"
+echo "####### Tessdata"
+git clone "https://github.com/tesseract-ocr/tessdata_best"
 cp "tessdata_best/eng.traineddata" "tesseract/tessdata/"
 
+echo "####### Start Training"
 PANGOCAIRO_BACKEND=fc \
 ~/projects/tesseract/training/tesstrain.sh \
   --fonts_dir /Library/Fonts \
